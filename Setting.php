f0d3a9b2774f90104f0c07f0d60db92ffeefc76c
Hendra Tandiono <Happy.hendratandiono@gmail.com>
2018-07-01 21:50:58 +0700

    UpdateCh

--- Setting.php
+++ Setting.php
@@ -1,9 +1,9 @@
 public function getChannelAccessToken(){
-	$channelAccessToken = "<YOUR CHANNEL ACCESS TOKEN>";
+	$channelAccessToken = "qok5zfpttYHz+w4qJHwQ2/0BHGHvgH6Lc8umSfLZIhpOTmI2Dg1qAbwYO3qcyBzvcAJFpSa3sOD9xthTRiBVWdW+B/6t2sJ7iNHEa2IdOSGB4ntwOat/9nXE/rX9cbaF0L4yOIkWghAZAgX7R6wylwdB04t89/1O/w1cDnyilFU=";
 	return $channelAccessToken;
 }
         
 public function getChannelSecret(){
-	$channelSecret = "<YOUR CHANNEL SECRET>";
+	$channelSecret = "e1a8ccfacec550e50fd32a335eb13930";
 	return $channelSecret;
 }
