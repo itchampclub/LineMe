48525f4b89f38be9e2975603c096dfc592b64fbd
Hendra Tandiono <Happy.hendratandiono@gmail.com>
2018-07-01 21:57:07 +0700

    fix

--- Setting.php
+++ Setting.php
@@ -1,4 +1,5 @@
 <?php
+
 class Setting {
 	public function getChannelAccessToken(){
 		$channelAccessToken = "qok5zfpttYHz+w4qJHwQ2/0BHGHvgH6Lc8umSfLZIhpOTmI2Dg1qAbwYO3qcyBzvcAJFpSa3sOD9xthTRiBVWdW+B/6t2sJ7iNHEa2IdOSGB4ntwOat/9nXE/rX9cbaF0L4yOIkWghAZAgX7R6wylwdB04t89/1O/w1cDnyilFU=";
