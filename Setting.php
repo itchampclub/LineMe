c79f51809f33a1201927aac3ac25445f42a1beaa
Hendra Tandiono <Happy.hendratandiono@gmail.com>
2018-07-01 21:55:02 +0700

    fix

--- Setting.php
+++ Setting.php
@@ -1,9 +1,19 @@
-public function getChannelAccessToken(){
-	$channelAccessToken = "qok5zfpttYHz+w4qJHwQ2/0BHGHvgH6Lc8umSfLZIhpOTmI2Dg1qAbwYO3qcyBzvcAJFpSa3sOD9xthTRiBVWdW+B/6t2sJ7iNHEa2IdOSGB4ntwOat/9nXE/rX9cbaF0L4yOIkWghAZAgX7R6wylwdB04t89/1O/w1cDnyilFU=";
-	return $channelAccessToken;
-}
-        
-public function getChannelSecret(){
-	$channelSecret = "e1a8ccfacec550e50fd32a335eb13930";
-	return $channelSecret;
+<?php
+class Setting {
+	public function getChannelAccessToken(){
+		$channelAccessToken = "qok5zfpttYHz+w4qJHwQ2/0BHGHvgH6Lc8umSfLZIhpOTmI2Dg1qAbwYO3qcyBzvcAJFpSa3sOD9xthTRiBVWdW+B/6t2sJ7iNHEa2IdOSGB4ntwOat/9nXE/rX9cbaF0L4yOIkWghAZAgX7R6wylwdB04t89/1O/w1cDnyilFU=";
+		return $channelAccessToken;
+	}
+	public function getChannelSecret(){
+		$channelSecret = "e1a8ccfacec550e50fd32a335eb13930";
+		return $channelSecret;
+	}
+	public function getApiReply(){
+		$api = "https://api.line.me/v2/bot/message/reply";
+		return $api;
+	}
+	public function getApiPush(){
+		$api = "https://api.line.me/v2/bot/message/push";
+		return $api;
+	}
 }
