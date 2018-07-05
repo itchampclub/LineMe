5e222f72a7deb5dde941797b9ca948a987ee6576
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-05 10:18:11 +0700

    Update Linebot.php

--- Linebot.php
+++ Linebot.php
@@ -1,82 +1,146 @@
+
 <?php
-if (!function_exists('hash_equals')) {
-    defined('USE_MB_STRING') or define('USE_MB_STRING', function_exists('mb_strlen'));
-    function hash_equals($knownString, $userString)
-    {
-        $strlen = function ($string) {
-            if (USE_MB_STRING) {
-                return mb_strlen($string, '8bit');
-            }
-            return strlen($string);
-        };
-        // Compare string lengths
-        if (($length = $strlen($knownString)) !== $strlen($userString)) {
-            return false;
-        }
-        $diff = 0;
-        // Calculate differences
-        for ($i = 0; $i < $length; $i++) {
-            $diff |= ord($knownString[$i]) ^ ord($userString[$i]);
-        }
-        return $diff === 0;
-    }
-}
-class LINEBotTiny
-{
-    public function __construct($channelAccessToken, $channelSecret)
-    {
-        $this->channelAccessToken = $channelAccessToken;
-        $this->channelSecret = $channelSecret;
-    }
-    public function parseEvents()
-    {
-        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-            http_response_code(405);
-            error_log("Method not allowed");
-            exit();
-        }
-        $entityBody = file_get_contents('php://input');
-        if (strlen($entityBody) === 0) {
-            http_response_code(400);
-            error_log("Missing request body");
-            exit();
-        }
-        if (!hash_equals($this->sign($entityBody), $_SERVER['HTTP_X_LINE_SIGNATURE'])) {
-            http_response_code(400);
-            error_log("Invalid signature value");
-            exit();
-        }
-        $data = json_decode($entityBody, true);
-        if (!isset($data['events'])) {
-            http_response_code(400);
-            error_log("Invalid request body: missing events property");
-            exit();
-        }
-        return $data['events'];
-    }
-    public function replyMessage($message)
-    {
-        $header = array(
-            "Content-Type: application/json",
-            'Authorization: Bearer ' . $this->channelAccessToken,
-        );
-        $context = stream_context_create(array(
-            "http" => array(
-                "method" => "POST",
-                "header" => implode("\r\n", $header),
-                "content" => json_encode($message),
-            ),
-        ));
-        $response = file_get_contents('https://api.line.me/v2/bot/message/reply', false, $context);
-        if (strpos($http_response_header[0], '200') === false) {
-            http_response_code(500);
-            error_log("Request failed: " . $response);
-        }
-    }
-    private function sign($body)
-    {
-        $hash = hash_hmac('sha256', $body, $this->channelSecret, true);
-        $signature = base64_encode($hash);
-        return $signature;
-    }
+	
+//	CREATE BY NONTACHAI KORNINAI
+//	01 OCTOBER 2016
+//
+//	UPDATE BY NONTACHAI KORNINAI
+//	25 MARCH 2018
+	
+require_once __DIR__ . '/setting.php';
+class Linebot {
+	private $channelAccessToken;
+	private $channelSecret;
+	private $webhookResponse;
+	private $webhookEventObject;
+	private $apiReply;
+	private $apiPush;
+	
+	public function __construct(){
+		$this->channelAccessToken = Setting::getChannelAccessToken();
+		$this->channelSecret = Setting::getChannelSecret();
+		$this->apiReply = Setting::getApiReply();
+		$this->apiPush = Setting::getApiPush();
+		$this->webhookResponse = file_get_contents('php://input');
+		$this->webhookEventObject = json_decode($this->webhookResponse);
+	}
+	
+	private function httpPost($api,$body){
+		$ch = curl_init($api); 
+		curl_setopt($ch, CURLOPT_POST, true); 
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body)); 
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array( 
+		'Content-Type: application/json; charser=UTF-8', 
+		'Authorization: Bearer '.$this->channelAccessToken)); 
+		$result = curl_exec($ch); 
+		curl_close($ch); 
+		return $result;
+	}
+	
+	public function reply($text){
+		$api = $this->apiReply;
+		$webhook = $this->webhookEventObject;
+		$replyToken = $webhook->{"events"}[0]->{"replyToken"}; 
+		$body["replyToken"] = $replyToken;
+		$body["messages"][0] = array(
+			"type" => "text",
+			"text"=>$text
+		);
+		
+		$result = $this->httpPost($api,$body);
+		return $result;
+	}
+	
+	public function push($body){
+		$api = $this->apiPush;
+		$result = $this->httpPost($api, $body);
+		return $result;
+    	}
+    	public function pushText($to, $text){
+		$body = array(
+		    'to' => $to,
+		    'messages' => [
+			array(
+			    'type' => 'text',
+			    'text' => $text
+			)
+		    ]
+		);
+		$this->push($body);
+	 }
+   	 public function pushImage($to, $imageUrl, $previewImageUrl = false){
+        	$body = array(
+		    'to' => $to,
+		    'messages' => [
+			array(
+			    'type' => 'image',
+			    'originalContentUrl' => $imageUrl,
+			    'previewImageUrl' => $previewImageUrl ? $previewImageUrl : $imageUrl
+			)
+		    ]
+		);
+		$this->push($body);
+    	}
+    	public function pushVideo($to, $videoUrl, $previewImageUrl){
+        	$body = array(
+          	  'to' => $to,
+          	  'messages' => [
+          	      array(
+			    'type' => 'video',
+			    'originalContentUrl' => $videoUrl,
+			    'previewImageUrl' => $previewImageUrl
+			)
+		    ]
+		);
+        	$this->push($body);
+    	}
+    	public function pushAudio($to, $audioUrl, $duration){
+		$body = array(
+		    'to' => $to,
+		    'messages' => [
+			array(
+			    'type' => 'audio',
+			    'originalContentUrl' => $audioUrl,
+			    'duration' => $duration
+			)
+		    ]
+		);
+		$this->push($body);
+	}
+    	public function pushLocation($to, $title, $address, $latitude, $longitude){
+		$body = array(
+		    'to' => $to,
+		    'messages' => [
+			array(
+			    'type' => 'location',
+			    'title' => $title,
+			    'address' => $address,
+			    'latitude' => $latitude,
+			    'longitude' => $longitude
+			)
+		    ]
+		);
+		$this->push($body);
+	}
+	
+	public function getMessageText(){
+		$webhook = $this->webhookEventObject;
+		$messageText = $webhook->{"events"}[0]->{"message"}->{"text"}; 
+		return $messageText;
+	}
+	
+	public function postbackEvent(){
+		$webhook = $this->webhookEventObject;
+		$postback = $webhook->{"events"}[0]->{"postback"}->{"data"}; 
+		return $postback;
+	}
+	
+	public function getUserId(){
+		$webhook = $this->webhookEventObject;
+		$userId = $webhook->{"events"}[0]->{"source"}->{"userId"}; 
+		return $userId;
+	}
+	
 }
