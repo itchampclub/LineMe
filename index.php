897b25809824b1a6547069bb559307067fbce494
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-05 10:20:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-  require_once __DIR__ . '/LineBot.php';
-  $bot = new Linebot();
-  $text = $bot->getMessageText();
-  //
-  $retext = "I am Shui Yin. Nice To meet You";
-  //
-  $bot->Reply($retext);
-?>
+require_once __DIR__ . '/lineBot.php';
+
+$bot = new Linebot();
+$text = $bot->getMessageText();
+$reply = "I am Reply you.";
+$bot->reply($reply);
