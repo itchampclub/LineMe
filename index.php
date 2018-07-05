ba7d53bf5c1e4e7b7a5734e2c8f0cbbab2a5686f
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-05 10:24:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-require_once __DIR__ . '/LineBot.php';
+require_once __DIR__ . '/Linebot.php';
 
 $bot = new Linebot();
 $text = $bot->getMessageText();
 $reply = "I am Reply you.";
 $bot->reply($reply);
+
+?>
