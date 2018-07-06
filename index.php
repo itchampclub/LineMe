f38563d2b99fcd51c9bf391346c0180782c49f62
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-06 13:35:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ $keytext = strtolower($arrtext[0]);
 //$text = "Farming Metal";
 
 echo "<br>".$text;
-echo "<br>".$arrtext;
-echo "<br>".$keytext;
 
 if($lentext >= 1){
-  echo "<br>".$lentext;
+  if($keytext == "sriend"){
+    echo "<br>".$keytext;
+    $bot->reply(file_get_contents('Info.txt'));
+  }
   /*
   if($keytext == "sriend"){
-    $bot->reply(file_get_contents('Info.txt'));
   }else{
     // Get Key
     $arrayme = explode("\n", file_get_contents($keytext."/key.txt"));
