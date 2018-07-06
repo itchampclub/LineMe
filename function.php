86cf02f4f20e422686da6fe17d313ea2ed104959
Hendra Tandiono <Happy.hendratandiono@gmail.com>
2018-07-06 21:41:05 +0700

    x

--- function.php
+++ function.php
@@ -1,10 +1,13 @@
 <?php
 
 function Checkmessage($userId,$text){
+
+	$bot->reply($text);
 	$file = "/Shuiyin/"$userId.".txt";
 	if(!file_exists($file)){
 		fwrite($file,$text);
 		$bot->reply("...");
+		return;
 	}
 }
 
