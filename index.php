8d1556276d107e77c26efe6c237d32a323e204ee
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-06 12:51:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,13 +9,13 @@ $arrtext = explode(" ", $text);
 if($arrtext[0] == "Sriend"){
   $bot->reply(file_get_contents('Info.txt'));
 }else{  
-  $loc = '/'.$arrtext[0].'/';
+  $loc = strtolower('/'.$arrtext[0].'/');
   echo "\n".$loc;
-  $arrayme = explode("\n", file_get_contents($loc.'Key.txt'));
+  $arrayme = explode("\n", file_get_contents($loc.'key.txt'));
   $num = array_search($text, $arrayme);
   echo "\nNumber = ".$num;
 
-  $arrayme = explode("~~~", file_get_contents($loc.'Info.txt'));
+  $arrayme = explode("~~~", file_get_contents($loc.'info.txt'));
   $result = $arrayme[$num];
   echo "\nResult = ".$result;
   $bot->reply($result);
