513979e8adfcc587de147963cf46f98e5f33b8ac
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-06 11:24:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ $bot = new Linebot();
 $text = $bot->getMessageText();
 
 $arrayme = explode("\n", file_get_contents('Keywords.txt'));
-echo "\n array[0] = ".$arrayme[0];
-$result = array_search("Sriend Help", $arrayme);
-echo "\n result = ".$result;
-//$bot->reply($result);
+$num = array_search($text, $arrayme);
+
+$arrayme = explode("===", file_get_contents('Information.txt'));
+$result = $arrayme[$num];
+$bot->reply($result);
 ?>
