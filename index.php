12f9f1aa7b9d67bc1b4384e570719e24f87f37c4
Hendra Tandiono <Happy.hendratandiono@gmail.com>
2018-07-06 20:23:15 +0700

    add Library

--- index.php
+++ index.php
@@ -16,17 +16,7 @@ if($lentext >= 1){
   if(strtolower($keytext) == "shuiyin"){
     //echo "<br>".$keytext;
     $bot->reply(file_get_contents('Info.txt'));
-  }else if(strtolower($keytext) == "recipe"){
-    $arrayme = explode("\n", file_get_contents(strtolower($keytext)."/key.txt"));
-    $num = array_search(str_replace($keytext." ","",$text), $arrayme);
-    //echo "<br>".str_replace($keytext." ","",$text);
-    $arrayme = explode("\n", file_get_contents(strtolower($keytext)."/info.txt"));
-    $arrtext = explode("\t",$arrayme[$num]);
-    //echo "<br>".$arrayme[$num];
-    $result = $arrtext[0]." [".$arrtext[1]."] [Lv ".$arrtext[2]." |Dif ".$arrtext[3]."]";
-    //echo "<br>".$result;
-    $bot->reply($result);
-  }else{
+  }else if(strtolower($keytext) == "farming"){
     $arrayme = explode("\n", file_get_contents($keytext."/key.txt"));
     $num = array_search($text, $arrayme);
     echo "<br>".$num;
@@ -34,6 +24,27 @@ if($lentext >= 1){
     $result = $arrayme[$num];
    // echo "<br>".$result;
     $bot->reply($result);
+  }else{
+    $text    = strtolower($text);
+    $keytext = strtolower($keytext);
+
+    $arrayme = explode("\n", file_get_contents($keytext."/key.txt"));
+    $num = array_search(str_replace($keytext." ","",$text), $arrayme);
+    $arrayme = explode("\n", file_get_contents($keytext."/info.txt"));
+    $arrtext = explode("\t",$arrayme[$num]);
+
+    switch ($keytext) {
+      case 'recipe':
+        $result  = $arrtext[0]." [".$arrtext[1]."] [Lv ".$arrtext[2]." |Dif ".$arrtext[3]."]\n";
+        $result .= "Materials: ".$arrtext[4]." ".$arrtext[5].", ".$arrtext[6]." ".$arrtext[7].", ".$arrtext[8]." ".$arrtext[9].", ".$arrtext[10]." ".$arrtext[11].".";
+        if($num > 0){$bot->reply($result);}
+        break;
+      case 'quest':
+        $result = "Quest Name: ".$arrtext[0];
+        if($num > 0){$bot->reply($result);}
+      default:
+        break;
+    }
   }
 }
 
