9b04e08f37075a4d7643f9a4e5dfcefa5e0e4ae7
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-06 13:17:21 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,19 +1,5 @@
 <?php
 
-function askSriend($ID,$Text,$Num){
+function askSriend(){
   $bot->reply(file_get_contents('Info.txt'));
-}
-
-function askFarming($Text,$Num){
-    if($Num == 1){
-      $result = file_get_contents(Text."/key.txt");
-    }else{
-      // Get Key
-      $arrayme = explode("\n", file_get_contents(Text."/key.txt"));
-      $num = array_search($text, $arrayme);
-      // Get Content
-      $arrayme = explode("~~~", file_get_contents(Text."/info.txt"));
-      $result = $arrayme[$num];
-    }
-    $bot->reply($result);  
 }
