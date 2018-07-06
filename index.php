0477205bc86d1a100c91f9fc60ca4eb0d3b2a8f5
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-06 13:08:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 require_once __DIR__ . '/Linebot.php';
+require_once __DIR__ . '/Function.php';
 
 $bot = new Linebot();
 $text = $bot->getMessageText();
 //$text = "Farming Metal";
 
 $arrtext = explode(" ", $text);
-if($arrtext[0] == "Sriend"){
-  $bot->reply(file_get_contents('Info.txt'));
-}else{  
-  $loc1 = strtolower($arrtext[0]."/key.txt");
-  $loc2 = strtolower($arrtext[0]."/info.txt");
-  // Get Key
-  $arrayme = explode("\n", file_get_contents($loc1));
-  $num = array_search($text, $arrayme);
-  // Get Content
-  $arrayme = explode("~~~", file_get_contents($loc2));
-  $result = $arrayme[$num];
-  $bot->reply($result);
+$lentext = count($arrtext);
+$keytext = $arrtext[0];
+$userid  = $bot->getUserID();
+
+switch($keytext){
+  case "Sriend":
+    askSriend($keytext,$lentext,$userid);
+  case "Farming":
+    askFarming($keytext);
+  default:
+    return 0;
 }
 ?>
