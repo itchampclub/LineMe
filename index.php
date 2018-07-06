cc59107fcf9c383be6fab8d171053797490a85a6
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-06 11:27:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $text = $bot->getMessageText();
 $arrayme = explode("\n", file_get_contents('Keywords.txt'));
 $num = array_search($text, $arrayme);
 
-$arrayme = explode("===", file_get_contents('Information.txt'));
+$arrayme = explode("~~~", file_get_contents('Information.txt'));
 $result = $arrayme[$num];
 $bot->reply($result);
 ?>
