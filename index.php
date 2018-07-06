87216b487d154456fc1d8fb6cf8234652ca9e7a1
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-06 11:41:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $text = $bot->getMessageText();
 
 $arrtext = explode(" ", $text);
 if($arrtext[0] == "Sriend"){
-  $bot->reply(file_get_contents('Key.txt'));
+  $bot->reply(file_get_contents('Info.txt'));
 }else{
   $loc = '/'.$arrtext[0].'/';
   $arrayme = explode("\n", file_get_contents($loc.'Key.txt'));
