df1c702841a85ee25532a30e5324f21b11614910
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-06 13:32:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,23 +15,21 @@ echo "<br>".$text;
 echo "<br>".$arrtext;
 echo "<br>".$lentext;
 echo "<br>".$keytext;
-/*
+
 if($lentext >= 1){
   if($keytext == "sriend"){
-  echo "<br> Success.";
     $bot->reply(file_get_contents('Info.txt'));
-  }else
-  if($keytext == "farming"){
-      // Get Key
-      $arrayme = explode("\n", file_get_contents($keytext."/key.txt"));
-      $num = array_search($text, $arrayme);
-      echo "<br>".$num;
-      // Get Content
-      $arrayme = explode("~~~", file_get_contents($keytext."/info.txt"));
-      $result = $arrayme[$num];
-      echo "<br>".$result;
-      bot->reply($result);
+  }else{
+    // Get Key
+    $arrayme = explode("\n", file_get_contents($keytext."/key.txt"));
+    $num = array_search($text, $arrayme);
+    echo "<br>".$num;
+    // Get Content
+    $arrayme = explode("~~~", file_get_contents($keytext."/info.txt"));
+    $result = $arrayme[$num];
+    echo "<br>".$result;
+    bot->reply($result);
   }
-}*/
+}
 
 ?>
