14d732becdefd77bca8bd9d396790e2f5f3fadc9
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-06 15:04:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,15 @@ if($lentext >= 1){
   if($keytext == "shuiyin"){
     //echo "<br>".$keytext;
     $bot->reply(file_get_contents('Info.txt'));
+  }else if($keytext == "recipe"){
+    $arrayme = explode("\n", file_get_contents($keytext."/key.txt"));
+    $num = array_search($text, $arrayme);
+    echo "<br>".$num;
+    $arrayme = explode("~~~", file_get_contents($keytext."/info.txt"));
+    $arrtext = explode("\t",$arrayme[$num]);
+    $result = $arrtext[0]." [".$arrtext[1]."] [Lv ".$arrtext[2]." |Dif ".$arrtext[3]."]";
+    echo "<br>".$result;
+    $bot->reply($result);
   }else{
     $arrayme = explode("\n", file_get_contents($keytext."/key.txt"));
     $num = array_search($text, $arrayme);
