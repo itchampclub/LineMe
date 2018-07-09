28e21c9f3aaa7c5cfe4c9f8f7dd3519eec86c6fc
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-09 11:30:05 +0700

    Update Linebot.php

--- Linebot.php
+++ Linebot.php
@@ -143,10 +143,17 @@ class Linebot {
 		return $userId;
 	}
 
-	public function getUserProfile($userId){
-		$api = "https://api.line.me/v2/bot/profile/".$userId."\ -H 'Authorization: Bearer ".$this->channelAccessToken."'";
+	public function getUserProfile($userProfile){
+		if ($userProfile->isSucceeded()) {
+		    $profile = $userProfile->getJSONDecodedBody();
+		    echo "<br>".$profile['displayName'];
+		    echo "<br>".$profile['pictureUrl'];
+		    echo "<br>".$profile['statusMessage'];
+		}
+	}
+	//$api = "https://api.line.me/v2/bot/profile/".$userId."\ -H 'Authorization: Bearer ".$this->channelAccessToken."'";
 		$result = $this->httpPost($api, $body);
 		echo "First Result: ".$result."<br>";
-		return $result;
+		return $profile['displayName'];
 	}	
 }
