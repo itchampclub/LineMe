23ef2906be074f764525a3c3c3d4957e10c5dbe9
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-09 09:08:51 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -17,6 +17,46 @@ class Setting{
     $api = "https://api.line.me/v2/bot/message/push";
     Return $api;
   }
+  public function getUserProfile($UserId){
+    $api = "https://api.line.me/v2/bot/profile/".$userId;
+    return $api;
+  }
+  /*
+  // Post
+  public function getAccessToken(){
+    $api = "https://api.line.me/v2/oauth/accessToken";
+    return $api;
+  }
+  public function getRevoke(){
+    $api = "https://api.line.me/v2/oauth/revoke";
+    return $api;
+  }
+  public function getMulticast(){
+    $api = "https://api.line.me/v2/bot/message/multicast";
+    return $api;
+  }
+  // Get
+  public function getMessage($messageId){
+    $api = "https://api.line.me/v2/bot/message/".$messageId."/content";
+    return $api;
+  }
+  public function getGroupUser($groupId,$userId){
+    $api = "https://api.line.me/v2/bot/group/".$groupId."/member/".$userId;
+    return $api;
+  }
+  public function getGroupMemberId($groupId){
+    $api = "https://api.line.me/v2/bot/group/".$groupId."/members/ids";
+    return $api;
+  }
+  public function getLeaveRequest($groupId){
+    $api = "https://api.line.me/v2/bot/group/".$groupId."/leave";
+    return $api;
+  }
+  public function getRoomUserProfile($roomId,$userId){
+    $api = "https://api.line.me/v2/bot/room/".$roomId."/member/".$userId;
+    return $api;
+  }
+  */
   
 }
 
