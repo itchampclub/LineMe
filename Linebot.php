baddfdbdc875ce239a69750b692f8014c4e7d8b8
Sriend <39609701+Sriend@users.noreply.github.com>
2018-07-09 11:34:33 +0700

    Update Linebot.php

--- Linebot.php
+++ Linebot.php
@@ -142,5 +142,16 @@ class Linebot {
 		$userId = $webhook->{"events"}[0]->{"source"}->{"userId"}; 
 		return $userId;
 	}
-
+	public function getUserProfile($userId){
+		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($this->channelAccessToken);
+		$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $this->channelSecret]);
+		$response = $bot->getProfile($userId);
+		if ($response->isSucceeded()) {
+		    $profile = $response->getJSONDecodedBody();
+		    echo $profile['displayName'];
+		    echo $profile['pictureUrl'];
+		    echo $profile['statusMessage'];
+		}
+		return $profile['displayName'];
+	}
 }
