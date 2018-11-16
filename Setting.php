bd9310b4c40d260d7353a67f801239afd3f98ddd
Ittirit Thipphinit <itchampclub@gmail.com>
2018-11-16 10:44:45 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -2,11 +2,11 @@
 
 class Setting{
   public function getChannelAccessToken(){
-    $channelAccessToken = "qok5zfpttYHz+w4qJHwQ2/0BHGHvgH6Lc8umSfLZIhpOTmI2Dg1qAbwYO3qcyBzvcAJFpSa3sOD9xthTRiBVWdW+B/6t2sJ7iNHEa2IdOSGB4ntwOat/9nXE/rX9cbaF0L4yOIkWghAZAgX7R6wylwdB04t89/1O/w1cDnyilFU=";
+    $channelAccessToken = "1B8pMUtZdLgdebgTuRrxV3YirCQv91mbXGnxvlTbX7Cxn471Fs0bBgwGVpedxnPKm7tZUWxnMrT2NqCBCLAG8L7r6vtYoZwb3iqRvYr3BZGrZX/mRNFG8lzNbLr5CHO4PWfTicerD5PVHYjC8mpQ4wdB04t89/1O/w1cDnyilFU=";
     Return $channelAccessToken;
   }
   public function getChannelSecret(){
-    $channelSecret = "e1a8ccfacec550e50fd32a335eb13930";
+    $channelSecret = "69b2d81ee6e8ff48d2cacdc8c7d8c337";
     Return $channelSecret;
   }
   public function getApiReply(){
